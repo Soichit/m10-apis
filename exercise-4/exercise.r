c742362e2f8e340e1fb15f9d95e87c61e98327b8
Soichi Tanabe <soichit@uw.edu>
2016-10-25 20:52:42 -0700

    finished 1-4

--- exercise-4/exercise.r
+++ exercise-4/exercise.r
@@ -2,29 +2,43 @@
 
 # Make a variable base.url that has the same base url from the omdb documentation.
 # (Hint: visit https://www.omdbapi.com/ to find the base url)
+base.url <- 'http://www.omdbapi.com/?'
 
 # Make a variable called movie that has the names of your favorite movie
+movie <- "Lord of the Rings: The Fellowship of the Ring"
 
-# Make a variable called move.no.spaces that holds movie where all the spaces
+# Make a variable called movie.no.spaces that holds movie where all the spaces
 # are replaced with the '+' character
+movie.no.spaces <- gsub(" ", "+", movie)
 
 # Make a variable called "parameters" that holds a string with the parameters
 # to pass to the API. View the OMDb documentation to see which parameters
 # are available. Remember to separate parameters with the '&' character.
-
+parameters <- paste0('t=', movie.no.spaces, '&', 'r=json')
 # Make a variable called request that is a string of a request URL made up of the base URL
 # and the parameters string
-
+request <- paste0(base.url, parameters)
+print(request)
 # Use fromJSON to retrieve JSON data from the omdb api using your request URL.
 # Store the result in a variable called movie.data
+movie.data <- fromJSON(request) 
 
 # Make movie_data into a data frame using as.data.frame
+movie.df <- as.data.frame(movie.data) 
+View(movie.df)
 
 # Write a function called Director that accepts a data frame of movie info and returns
 # A vector of strings that states a movie and the director of said movie.
+Director <- function(movie.df) {
+  result <- paste(movie.df$Title, "was directed by", movie.df$Director)
+  return (result)
+}
 
 # Call Director with your favorite movie, and assign it to the variable movie.director
+movie.director <- Director(movie.df)
+paste(movie.director)
 
+  
 # Bonus 
 
 # Knowing the director of on movie is okay, but it'd be great to know the directors of different
